056e4879f5ea5f87ae24475d7202278b3d3fd6a5
李志强 <lizhiqiang1@msunsoft.com>
2019-08-14 16:50:47 +0800

    测试成功

--- testCsharp/Form1.cs
+++ testCsharp/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using Emgu.CV;
+using Emgu.CV.Structure;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -16,7 +18,7 @@ namespace testCsharp
         [DllImport(@"F:\github\libfacedetection\sln\Debug\SfxFaceDetect.dll", CallingConvention = CallingConvention.Cdecl,SetLastError = true)]
         public static extern int add(int a, int b);
         [DllImport(@"F:\github\libfacedetection\sln\Debug\SfxFaceDetect.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-        public static extern IntPtr sfxFaceDetect(IntPtr result_buffer, IntPtr rgb_image_data, int width, int height, int step);
+        unsafe public static extern int* sfxFaceDetect(IntPtr result_buffer, IntPtr rgb_image_data, int width, int height, int step);
 
 
         const int DETECT_BUFFER_SIZE = 0x20000;
@@ -30,24 +32,44 @@ namespace testCsharp
             openPic();
             //test();
         }
-        private void openPic()
+        unsafe private void openPic()
         {
 
 
             if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                this.imageBox1.Load(this.openFileDialog1.FileName);
 
+                String filename = this.openFileDialog1.FileName;
+                var Image = new Image<Bgr, byte>(filename);
+
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
                 //MessageBox.Show(r.ToString());
                 //申请非托管指针
                 IntPtr result_buffer = Marshal.AllocHGlobal(sizeof(byte) * DETECT_BUFFER_SIZE);
+                //byte[] result_buffer = new byte[DETECT_BUFFER_SIZE];
 
-                var rgb_image_data = this.imageBox1.Image.Ptr;
-                Mat mat =(Mat)this.imageBox1.Image;
- 
 
-                IntPtr result = sfxFaceDetect(result_buffer, rgb_image_data, mat.Cols, mat.Rows, mat.Step);
+                Mat mat =Image.Mat;
+                int* pResults = sfxFaceDetect(result_buffer, mat.DataPointer, mat.Cols, mat.Rows, mat.Step);
+                for (int i = 0; i < *pResults; i++)
+                {
+                    short* p = ((short*)(pResults + 1)) + 142 * i;
+                    int x = p[0];
+                    int y = p[1];
+                    int w = p[2];
+                    int h = p[3];
+                    int confidence = p[4];
+                    int angle = p[5];
 
+                    //printf("face_rect=[%d, %d, %d, %d], confidence=%d, angle=%d\n", x, y, w, h, confidence, angle);
+                    //rectangle(result_cnn, Rect(x, y, w, h), Scalar(0, 255, 0), 2);
+                    Rectangle face = new Rectangle(x, y, w, h);
+                    CvInvoke.Rectangle(mat, face, new Bgr(Color.Red).MCvScalar, 2);
+                }
+                this.imageBox1.Image = mat.Clone();
+                watch.Stop();
+                System.Diagnostics.Trace.WriteLine("耗时:" + watch.ElapsedMilliseconds + "ms");
                 //释放指针
                 Marshal.FreeHGlobal(result_buffer);
             }
